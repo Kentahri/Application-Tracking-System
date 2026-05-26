679ed7c9e6646088cbfd5b1451727bbad64f2433
Kentahri <anhquan47eahleo@gmail.com>
2026-05-26 13:19:08 +0700

    ignore import *

--- src/main/java/ats/controller/DepartmentController.java
+++ src/main/java/ats/controller/DepartmentController.java
@@ -6,9 +6,15 @@ import ats.dto.department.DepartmentResponse;
 import ats.dto.department.DepartmentUpdateRequest;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.web.bind.annotation.*;
-import quan.ats.dto.department.*;
 import ats.service.DepartmentService;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
