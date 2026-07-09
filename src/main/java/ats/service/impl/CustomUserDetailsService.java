306fca50fa37257fbef0da344c509840e32b10f0
Haichan <160374468+DaeyangTQH@users.noreply.github.com>
2026-07-09 16:14:39 +0700

    feat(auth): add candidate login functionality and user details handling

--- src/main/java/ats/service/impl/CustomUserDetailsService.java
+++ src/main/java/ats/service/impl/CustomUserDetailsService.java
@@ -1,6 +1,8 @@
 package ats.service.impl;
 
+import ats.entity.Candidate;
 import ats.entity.User;
+import ats.repository.CandidateRepository;
 import ats.repository.UserRepository;
 import lombok.AllArgsConstructor;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,14 +15,20 @@ import org.springframework.stereotype.Service;
 public class CustomUserDetailsService implements UserDetailsService {
 
     private final UserRepository userRepository;
+    private final CandidateRepository candidateRepository;
 
     @Override
     public UserDetails loadUserByUsername(String email) {
         User user = userRepository.findByEmail(email);
-        if(user == null){
-            throw new UsernameNotFoundException("User not found with email: " + email);
+        if (user != null) {
+            return new CustomUserDetails(user);
         }
 
-        return new CustomUserDetails(user);
+        Candidate candidate = candidateRepository.findByEmail(email);
+        if (candidate != null) {
+            return new CandidateUserDetails(candidate);
+        }
+
+        throw new UsernameNotFoundException("Account not found with email: " + email);
     }
 }
