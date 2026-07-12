e97fc5729dab57514c848ff81a2620641443c6a9
VuietKie <vuhiep27082005@gmail.com>
2026-07-12 17:31:13 +0700

    change password for candidate

--- src/main/java/ats/service/CandidateService.java
+++ src/main/java/ats/service/CandidateService.java
@@ -1,9 +1,14 @@
 package ats.service;
 
+import ats.dto.candidate.CandidateChangePasswordRequest;
 import ats.dto.candidate.CandidateRequest;
 import ats.dto.candidate.CandidateResponse;
+
+import java.security.Principal;
 
 public interface CandidateService {
 
     CandidateResponse create(CandidateRequest request);
+
+    CandidateResponse changePassword(Long id, CandidateChangePasswordRequest request, Principal principal);
 }
